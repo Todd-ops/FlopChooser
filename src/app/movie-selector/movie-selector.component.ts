5ac694a7764d617037d5ae7f63cfe08bf2426265
Todd-ops <toddpatton84@gmail.com>
2022-01-05 16:33:17 -0600

    4 files modified

--- src/app/movie-selector/movie-selector.component.ts
+++ src/app/movie-selector/movie-selector.component.ts
@@ -3,7 +3,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-movie-selector',
   templateUrl: './movie-selector.component.html',
-  styleUrls: ['./movie-selector.component.css']
+  styles: [
+    'div { cursor:pointer; }',
+    'img {width:8rem; height:auto}'
+  ]
 })
 export class MovieSelectorComponent {
   @Input() movie;
